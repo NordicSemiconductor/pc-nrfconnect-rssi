14df1d35b5de67445d7ab6377c33280d2d32f8b7
Iván Sánchez Ortega <ivan@sanchezortega.es>
2018-01-12 16:37:34 +0100

    Don't assume serialport.isOpen is a function (for forwards compatibility)

--- actions/rssiActions.js
+++ actions/rssiActions.js
@@ -152,7 +152,7 @@ export function open(serialPort) {
 
 export function close() {
     return dispatch => {
-        if (port && port.isOpen()) {
+        if (port && port.isOpen && (typeof (port.isOpen) !== 'function' || port.isOpen())) {
             stopReading();
             dispatch(rssiData());
             port.close(() => {
