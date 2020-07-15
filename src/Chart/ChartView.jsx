de47d32ec397c3a997a4613a4accddc5b1d274ab
Marko Schulz <Marko.Schulz@nordicsemi.no>
2020-07-15 17:48:21 +0200

    Simplify listing of BLE channels
    Instead of doing some nifty logic, let us just list them verbatim.

--- src/Chart/ChartView.jsx
+++ src/Chart/ChartView.jsx
@@ -42,10 +42,12 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 Chart.plugins.register(ChartDataLabels);
 
-const bleChannels = Array.from(Array(37), (_, x) => x);
-bleChannels.unshift(37);
-bleChannels.splice(12, 0, 38);
-bleChannels.push(39);
+const bleChannels = [
+    '37', '00', '01', '02', '03', '04', '05', '06', '07', '08', '09', '10',
+    '38', '11', '12', '13', '14', '15', '16', '17', '18', '19', '20', '21',
+    '22', '23', '24', '25', '26', '27', '28', '29', '30', '31', '32', '33',
+    '34', '35', '36', '39',
+];
 
 // Official Nordic colors, taken from https://github.com/NordicSemiconductor/pc-nrfconnect-shared/blob/9bb1f72849/src/variables.scss
 const blueSlate = '#0033a0';
@@ -67,9 +69,6 @@ const color = {
         background: gray50,
     },
 };
-
-const interlace = arr => arr.map(v => v.toString().padStart(2, '0'));
-const bleChannelTicks = interlace(bleChannels);
 
 const rssiColors = Array(40).fill(color.bar.normal);
 const rssiMaxColors = Array(40).fill(color.bar.normalMax);
@@ -127,7 +126,7 @@ const ChartView = ({
                         position: 'top',
                         offset: true,
                         ticks: {
-                            callback: (_, index) => bleChannelTicks[index],
+                            callback: (_, index) => bleChannels[index],
                             minRotation: 0,
                             maxRotation: 0,
                             labelOffset: 0,
