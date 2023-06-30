01e7f8163bb5b1bfc7ae4e0cd450473f41232743
Kyle Micallef Bonnici <kyle.bonnici@nordicsemi.no>
2023-06-30 09:58:36 +0200

    Revert

--- src/appReducer.ts
+++ src/appReducer.ts
@@ -4,17 +4,14 @@
  * SPDX-License-Identifier: LicenseRef-Nordic-4-Clause
  */
 
-import {
-    NrfConnectState,
-    RootState as SharedRootState,
-} from 'pc-nrfconnect-shared';
+import { NrfConnectState } from 'pc-nrfconnect-shared';
 import { combineReducers } from 'redux';
 
 import rssiReducer from './features/rssiSlice';
 
 type AppState = ReturnType<typeof appReducer>;
 
-export type RootState = NrfConnectState<AppState & SharedRootState>;
+export type RootState = NrfConnectState<AppState>;
 
 const appReducer = combineReducers({
     rssi: rssiReducer,
