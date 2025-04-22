69593956e20e0f169e90e9a8dd8493f2526a30fe
Svyatoslav <kiev.devel@gmail.com>
2025-04-22 12:02:15 +0200

    update shared; update engine

--- src/app/DeviceSelector.tsx
+++ src/app/DeviceSelector.tsx
@@ -6,10 +6,7 @@
 
 import React from 'react';
 import { useDispatch } from 'react-redux';
-import {
-    DeviceSelector,
-    logger,
-} from '@nordicsemiconductor/pc-nrfconnect-shared';
+import { DeviceSelector } from '@nordicsemiconductor/pc-nrfconnect-shared';
 import { DeviceTraits } from '@nordicsemiconductor/pc-nrfconnect-shared/nrfutil/device';
 
 import {
@@ -32,14 +29,7 @@ export default () => {
         <DeviceSelector
             deviceSetupConfig={deviceSetupConfig}
             deviceListing={deviceListing}
-            onDeviceConnected={device =>
-                logger.info(`Device Connected SN:${device.serialNumber}`)
-            }
-            onDeviceDisconnected={device =>
-                logger.info(`Device Disconnected SN:${device.serialNumber}`)
-            }
             onDeviceIsReady={device => {
-                logger.info(`Device isReady SN:${device.serialNumber}`);
                 dispatch(openDevice(device));
             }}
             onDeviceDeselected={() => {
